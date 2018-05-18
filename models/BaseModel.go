3e34910f3f31d9fcbd252e9b92534d660d900719
Administrator <wangshiyang@aliyun.com>
2018-05-18 18:35:15 +0800

    init

---
+++ models/BaseModel.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+var db *gorm.DB
+
+type Model struct {
+
+}
---
+++ models/cargo/cargo.go
@@ -0,0 +1,5 @@
+package cargo
+
+// TrackingID 是 cargo表的 唯一主键
+type TrackingID string
+
